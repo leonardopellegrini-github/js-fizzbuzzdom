3d8e92a2be8072e983dfba6d084ad20497c671e6
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-04-08 17:44:56 +0200

    inserimento commenti

--- js/script.js
+++ js/script.js
@@ -15,31 +15,35 @@ const limit = 100;
 //inserisco il FOR per inizializzare l'operazione
 for(let i = 1; i <= limit; i++){
 
-  //se i è multiplo di 3 allora stampo
+  //inserisco l'oggetto div
+  const div = document.createElement('div');
+
+  //è divisibile per entrambi?
   if ((i % 3 == 0) && (i % 5 == 0)){
     console.log(i);
-    const div = document.createElement('div');
     div.className = 'box giallo';
     div.append('FIZZ BUZZ');
     container.append(div);
-  } else if (i % 5 == 0){
+  } 
+  //è divisibile per 5?
+  else if (i % 5 == 0){
     console.log(i);
-    const div = document.createElement('div');
     div.className = 'box verde';
     div.append('BUZZ');
     console.log(div);
     container.append(div);
-  } else if (i % 3 == 0){
+  } 
+  //è divisibile per 3?
+  else if (i % 3 == 0){
     console.log(i);
-    const div = document.createElement('div');
     div.className = 'box viola';
     div.append('FIZZ');
     console.log(div);
     container.append(div);
   }
+  //altrimenti stampa il numero
   else{
     console.log(i);
-    const div = document.createElement('div');
     div.className = 'box rosso';
     div.append(i);
     console.log(div);
